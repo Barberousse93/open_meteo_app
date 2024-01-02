4bd32b2ff372ad3af7d17576a5dbd32d61b68b78
Alain BOUCHERY <alain.bouchery@enedis.fr>
2024-01-02 16:54:41 +0100

    Responsiveness

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ import Rain from '@mui/icons-material/WaterDrop'
 import Snow from '@mui/icons-material/AcUnit'
 import Wind from '@mui/icons-material/Air'
 import NavigationIcon from '@mui/icons-material/Navigation'
-import { Tooltip } from '@mui/material'
+import { Grid, Tooltip, Typography } from '@mui/material'
 
 export default function Card(datas) {
   const [dateAAfficher, setDateAAfficher] = useState(null)
@@ -44,57 +44,63 @@ export default function Card(datas) {
   }, [cardDatas.time])
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        marginBottom: '10px',
-      }}
-    >
-      <div>{dateAAfficher}</div>
-      <Tooltip
-        arrow
-        title={weather.description}
-        slotProps={{
-          popper: {
-            modifiers: [
-              {
-                name: 'offset',
-                options: {
-                  offset: [0, -40],
+    <Grid container spacing={2} alignItems='center'>
+      <Grid item xs={12} sm={12} md={12} lg={2} style={{ textAlign: 'center' }}>
+        <Typography
+          fontSize={{
+            lg: 20,
+            md: 20,
+            sm: 25,
+            xs: 30,
+          }}
+        >
+          {dateAAfficher}
+        </Typography>
+      </Grid>
+      <Grid item xs={6} sm={4} lg={2} style={{ textAlign: 'center' }}>
+        <Tooltip
+          arrow
+          title={weather.description}
+          slotProps={{
+            popper: {
+              modifiers: [
+                {
+                  name: 'offset',
+                  options: {
+                    offset: [0, -40],
+                  },
                 },
-              },
-            ],
-          },
-        }}
-      >
-        <img src={weather.image} />
-      </Tooltip>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              ],
+            },
+          }}
+        >
+          <img src={weather.image} />
+        </Tooltip>
+      </Grid>
+      <Grid item xs={6} sm={4} lg={2} style={{ textAlign: 'center' }}>
         <Thermometre /> {cardDatas.temperatureMin}°C / {cardDatas.temperatureMax}°C
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      </Grid>
+
+      <Grid item xs={6} sm={4} lg={2} style={{ textAlign: 'center' }}>
         <SunsetSunrise /> {sunSetRise.sunrise} / {sunSetRise.sunset}
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      </Grid>
+
+      <Grid item xs={6} sm={6} md={6} lg={2} style={{ textAlign: 'center' }}>
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <Rain /> {cardDatas.precipitationProbability}%
           {cardDatas.precipitationProbability > 0 ? <div>{cardDatas.rainSum}mm</div> : null}
         </div>
-
         {cardDatas.snowfallSum > 0 ? (
           <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Snow /> {cardDatas.snowfallSum}cm
           </div>
         ) : null}
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      </Grid>
+      <Grid item xs={12} sm={6} md={6} lg={2} style={{ textAlign: 'center' }}>
         <Wind /> {cardDatas.windSpeedMax}km/h{' '}
         <NavigationIcon sx={{ transform: `rotate(${correctedDirection}deg)`, ml: 1 }} /> (
         {cardDatas.windGustsMax}km/h)
-      </div>
-    </div>
+      </Grid>
+    </Grid>
   )
 }
--- src/components/current.jsx
+++ src/components/current.jsx
@@ -27,7 +27,11 @@ function Current(props) {
     townInfo &&
     townInfo.townName && (
       <Paper
-        sx={{ padding: '30px', backgroundColor: theme.palette.primary.dark, borderRadius: '20px' }}
+        sx={{
+          padding: '30px',
+          backgroundColor: theme.palette.primary.dark,
+          borderRadius: '20px',
+        }}
       >
         <Typography variant='p'>Actuellement</Typography>
         <Divider sx={{ color: theme.palette.primary.dark }} />
@@ -40,19 +44,56 @@ function Current(props) {
             justifyContent: 'space-between',
           }}
         >
-          <Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
             <div style={{ display: 'flex', flexDirection: 'row' }}>
-              <Typography variant='h1' sx={{ fontWeight: 'bolder' }}>
+              <Typography
+                variant='h1'
+                sx={{ fontWeight: 'bolder' }}
+                fontSize={{
+                  lg: 100,
+                  md: 80,
+                  sm: 60,
+                  xs: 60,
+                }}
+              >
                 {current.temperature_2m}
               </Typography>
-              <Typography variant='h3' sx={{ fontWeight: 'normal' }}>
+              <Typography
+                variant='h3'
+                sx={{ fontWeight: 'normal' }}
+                fontSize={{
+                  lg: 50,
+                  md: 40,
+                  sm: 30,
+                  xs: 30,
+                }}
+              >
                 °C
               </Typography>
-              <Typography variant='h6' sx={{ alignSelf: 'end' }}>
+              <Typography
+                variant='h6'
+                sx={{ alignSelf: 'end' }}
+                fontSize={{
+                  lg: '20px',
+                  md: 20,
+                  sm: 15,
+                  xs: 12,
+                }}
+              >
                 Ressenti : {current.apparent_temperature}°C
               </Typography>
             </div>
-            <Typography variant='h5'>{weatherCode.description}</Typography>
+            <Typography
+              variant='h5'
+              fontSize={{
+                lg: 35,
+                md: 30,
+                sm: 25,
+                xs: 20,
+              }}
+            >
+              {weatherCode.description}
+            </Typography>
           </Box>
           <img src={weatherCode.image} alt={weatherCode.description} width='150px' height='150px' />
         </Box>
@@ -61,6 +102,12 @@ function Current(props) {
           <Typography
             variant='h4'
             sx={{ fontFamily: 'Roboto', marginTop: '30px', marginBottom: '30px' }}
+            fontSize={{
+              lg: 45,
+              md: 40,
+              sm: 35,
+              xs: 30,
+            }}
           >
             {townInfo.townName}
           </Typography>
@@ -69,32 +116,85 @@ function Current(props) {
             target='_blank'
             underline='hover'
             sx={{ color: '#0f0', alignSelf: 'center', marginLeft: '20px' }}
+            fontSize={{
+              lg: 20,
+              md: 20,
+              sm: 15,
+              xs: 15,
+            }}
           >
             Carte
           </Link>
         </div>
         {/*  */}
         <div>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '200px' }}>
-            <div
-              style={{ display: 'flex', alignContent: 'center', justifyContent: 'space-around' }}
+          <Box
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+
+              width: '250px',
+            }}
+          >
+            <Box
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-around',
+              }}
             >
-              <Wind sx={{ m1: 1, fontSize: '35px' }} />
-              <Typography variant='h6'>{current.wind_speed_10m}km/h</Typography>
+              <Wind
+                sx={{
+                  m1: 1,
+                  fontSize: 35,
+                  color: 'gray',
+                  fontSize: {
+                    lg: 35,
+                    md: 30,
+                    sm: 25,
+                    xs: 25,
+                  },
+                }}
+              />
+              <Typography
+                variant='h6'
+                fontSize={{
+                  lg: 30,
+                  md: 25,
+                  sm: 25,
+                  xs: 20,
+                }}
+              >
+                {current.wind_speed_10m}km/h
+              </Typography>
               <span>
                 <NavigationIcon
                   sx={{
                     transform: `rotate(${current.wind_direction_10m}deg)`,
                     ml: 1,
-                    fontSize: '35px',
+                    fontSize: {
+                      lg: 35,
+                      md: 30,
+                      sm: 25,
+                      xs: 25,
+                    },
                   }}
                 />
               </span>
-            </div>
-            <Typography variant='h6' sx={{ alignSelf: 'center' }}>
+            </Box>
+            <Typography
+              variant='h6'
+              sx={{ alignSelf: 'center' }}
+              fontSize={{
+                lg: 25,
+                md: 20,
+                sm: 20,
+                xs: 15,
+              }}
+            >
               (Rafales : {current.wind_gusts_10m}km/h)
             </Typography>
-          </div>
+          </Box>
           <p style={{ width: '100%', textAlign: 'right', fontSize: '10px' }}>{current.time}</p>
         </div>
       </Paper>
